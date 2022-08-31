1d591cc5de240331f4c00ce66bfee1f2afbf9fe5
raimarsilva <106407456+raimarsilva@users.noreply.github.com>
2022-08-31 20:28:19 -0300

    subindo o projeto de pratica em react

--- Pratica/src/main/java/tst/trabalho/pratica/models/Processo.java
+++ Pratica/src/main/java/tst/trabalho/pratica/models/Processo.java
@@ -11,8 +11,7 @@ import lombok.Data;
 @Data @Entity @Table(name="tb_processos")
 public class Processo {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private final long idProcesso=1L;
 	private String numeroProcesso;
 	private String relator;
