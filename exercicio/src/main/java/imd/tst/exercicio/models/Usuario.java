2b33692f29d030add5ff838e07e0c7630a1e153f
raimarsilva <106407456+raimarsilva@users.noreply.github.com>
2022-09-21 13:37:31 -0300

    fix: atualizada a classe Usuario

--- exercicio/src/main/java/imd/tst/exercicio/models/Usuario.java
+++ exercicio/src/main/java/imd/tst/exercicio/models/Usuario.java
@@ -41,43 +41,36 @@ public class Usuario implements Serializable, UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
 		return this.roles ;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
 		return this.senha;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return this.nome;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	
