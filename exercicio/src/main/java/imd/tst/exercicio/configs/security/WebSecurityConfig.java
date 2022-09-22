9eb5447474d020167598648431541364c30ffe7e
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-09-22 13:12:57 -0300

    fix: aberto o acesso para cadastrar novo usuário

--- exercicio/src/main/java/imd/tst/exercicio/configs/security/WebSecurityConfig.java
+++ exercicio/src/main/java/imd/tst/exercicio/configs/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package imd.tst.exercicio.configs.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -9,7 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -17,25 +17,26 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class WebSecurityConfig {
 	
-	private final JwtService jwtService;
+	// private final JwtService jwtService;
 
-	private final UsuarioServiceImpl usuarioService;
+	// private final UsuarioServiceImpl usuarioService;
 
-	@Bean
-    public OncePerRequestFilter jwtFilter(){
-        return new JwtAuthFilter(jwtService, usuarioService);
-    }
+	// @Bean
+    // public OncePerRequestFilter jwtFilter(){
+    //     return new JwtAuthFilter(jwtService, usuarioService);
+    // }
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		//aqui vão as configurações de segurança:
 		http
-		.httpBasic()
-		.and()
-		.authorizeHttpRequests().anyRequest().authenticated()
-		.and().csrf().disable();
-		
-		return http.build();
+            .csrf().disable()
+            .authorizeHttpRequests((authz) -> authz
+                .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                    .permitAll()
+                .anyRequest().authenticated()   
+            )
+            .httpBasic();
+        return http.build();
 	}
 	
 	
