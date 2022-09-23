baebb3335414a6140c573d6b48760570d30c35fd
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-09-23 13:33:04 -0300

    doc: adicionadas descrições para os controller de juiz e órgão

--- exercicio/src/main/java/imd/tst/exercicio/controller/JuizController.java
+++ exercicio/src/main/java/imd/tst/exercicio/controller/JuizController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 import imd.tst.exercicio.services.JuizService;
 import imd.tst.exercicio.models.Juiz;
@@ -27,12 +29,14 @@ public class JuizController {
 
     //Rota para listar todos
     @GetMapping(path = "/juizes")
+    @Operation(summary = "Listar todos os juizes", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<List<Juiz>> listarTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(juizService.listarTodos());
     }
 
     //Rota para buscar pelo id
     @GetMapping(path = "/juiz/{id}")
+    @Operation(summary = "Buscar juiz pelo id", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Juiz> buscarPeloId(@PathVariable Long id){
         Juiz juiz = juizService.buscarPeloId(id);
         if(juiz != null){
@@ -45,6 +49,7 @@ public class JuizController {
 
     // Rota para Atualizar
     @PatchMapping(path = "/juiz/{id}")
+    @Operation(summary = "Atualizar juiz", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Juiz> atualizar(@PathVariable Long id, @RequestBody Juiz juiz){
         Juiz juizAtualizado = juizService.atualizar(id, juiz);
         if(juizAtualizado != null){
--- exercicio/src/main/java/imd/tst/exercicio/controller/OrgaoController.java
+++ exercicio/src/main/java/imd/tst/exercicio/controller/OrgaoController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 import imd.tst.exercicio.services.OrgaoService;
 import imd.tst.exercicio.models.Orgao;
 import imd.tst.exercicio.models.dto.*;;
@@ -30,18 +33,21 @@ public class OrgaoController {
 
     // Rota para salvar
     @PostMapping(path = "/orgao")
+    @Operation(summary = "Salvar novo órgão", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Orgao> salvar(@RequestBody CadastrarOrgaoDTO orgaoDTO){
         return ResponseEntity.status(HttpStatus.CREATED).body(orgaoService.salvar(orgaoDTO));
     }
 
     // Rota para listar todos
     @GetMapping(path = "/orgaos")
+    @Operation(summary = "Listar todos os órgãos", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<List<Orgao>> listarTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(orgaoService.listarTodos());
     }
 
     // Rota para buscar pelo id
     @GetMapping(path = "/orgao/{id}")
+    @Operation(summary = "Buscar órgão pelo id", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Orgao> buscarPeloId(@PathVariable Long id){
         Orgao orgao = orgaoService.buscarPeloId(id);
         if(orgao != null){
@@ -53,6 +59,7 @@ public class OrgaoController {
 
     // Rota para Deletar
     @DeleteMapping(path = "/orgao/{id}")
+    @Operation(summary = "Deletar orgão pelo id", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<?> deletarPeloId(@PathVariable Long id){
         if(orgaoService.deletarPeloId(id)){
             return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso");
@@ -63,6 +70,7 @@ public class OrgaoController {
 
     // Rota para Atualizar
     @PatchMapping(path = "/orgao/{id}")
+    @Operation(summary = "Atualizar órgão", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Orgao> atualizar(@PathVariable Long id, @RequestBody Orgao orgao){
         Orgao orgaoAtualizado = orgaoService.atualizar(id, orgao);
         if(orgaoAtualizado != null){
