f4444289ce651286215eb075ff047c8986c17c97
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-09-23 12:38:09 -0300

    service para buscar juiz por id

---
+++ exercicio/src/main/java/imd/tst/exercicio/services/JuizService.java
@@ -0,0 +1,27 @@
+package imd.tst.exercicio.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import imd.tst.exercicio.models.Juiz;
+import imd.tst.exercicio.repository.JuizRepository;
+
+@Service
+public class JuizService {
+	
+	@Autowired
+    JuizRepository juizRepository;
+    
+    public Juiz buscarPeloId(Long id){
+        if(juizRepository.findById(id).isPresent()) {
+
+            return juizRepository.findById(id).get();
+        }else{
+            return null;
+        }
+
+    }
+
+}
