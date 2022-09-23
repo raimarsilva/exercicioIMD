8717d92ee199d2e61783ecfa208582149ab77b01
jldms <lucastr140@gmail.com>
2022-09-23 12:05:56 -0300

    alteraçõesget id  juiz

--- exercicio/src/main/java/imd/tst/exercicio/controller/JuizController.java
+++ exercicio/src/main/java/imd/tst/exercicio/controller/JuizController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,6 +27,18 @@ public class JuizController {
     @GetMapping(path = "/juizes")
     public ResponseEntity<List<Juiz>> listarTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(juizService.listarTodos());
+    }
+
+    //Rota para buscar pelo id
+    @GetMapping(path = "/juiz/{id}")
+    public ResponseEntity<Juiz> buscarPeloId(@PathVariable Long id){
+        Juiz juiz = juizService.buscarPeloId(id);
+        if(juiz != null){
+            System.out.println(juiz.toString());
+            return ResponseEntity.status(HttpStatus.OK).body(juiz);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(juiz);
+        }
     }
     
 }
--- exercicio/src/main/java/imd/tst/exercicio/services/JuizService.java
+++ exercicio/src/main/java/imd/tst/exercicio/services/JuizService.java
@@ -1,16 +1,12 @@
 package imd.tst.exercicio.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import imd.tst.exercicio.repository.JuizRepository;
-
-import java.util.List;
-
-
-
 import imd.tst.exercicio.models.Juiz;
-
+import imd.tst.exercicio.repository.JuizRepository;
 
 @Service
 public class JuizService {
@@ -18,8 +14,16 @@ public class JuizService {
     @Autowired
     JuizRepository juizRepository;
 
+    
     public List<Juiz> listarTodos(){
         return juizRepository.findAll();
     }
     
+    public Juiz buscarPeloId(Long id ){
+        if(juizRepository.findById(id).isPresent()){
+            return juizRepository.findById(id).get();
+        }else{
+            return null;
+        }
+    }
 }
